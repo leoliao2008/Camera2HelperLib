b5e6536298d41a8a2e69eff5246a41354fd071d5
leoliao2008 <leoliao2008@163.com>
2018-12-04 18:07:07 +0800

    company commit.

---
+++ LibraryCameraView/src/main/java/tgi/com/librarycameraview/CamerModule.java
@@ -0,0 +1,13 @@
+package tgi.com.librarycameraview;
+
+/**
+ * <p><b>Author:</b></p>
+ * <i>leo</i>
+ * <p><b>Date:</b></p>
+ * <i>On 4/12/2018</i>
+ * <p><b>Project:</b></p>
+ * <i>AndroidCameraDemo</i>
+ * <p><b>Description:</b></p>
+ */
+public class CamerModule {
+}
